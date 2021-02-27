3d2d46e05181a6319d7293e192da056371872956
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-02-27 11:52:55 -0500

    seeds, keep forgetting ;

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,8 +11,8 @@ INSERT INTO dog (client_name, dog_name, breed, age, food_requirements, friendlin
 VALUES ("alain", "huskiesRock", "husky", 2 , "kibble", 4);
 
 
-INSERT INTO (account, password, admin)
+INSERT INTO (account, password, admin);
 VALUES ("gkf",132, false);
 
-INSERT INTO (account, password, admin)
+INSERT INTO (account, password, admin);
 VALUES ("gkf_admin",123, true);
