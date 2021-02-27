a9d4d5a787d243a1d4bf6c45fb0274641e305f51
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-02-27 11:50:54 -0500

    bcrypt issue resolved

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,3 @@
-// const mysql = require('mysql');
-// require("dotenv").config();
-
-// const connection = mysql.createConnection({
-//     host:'localhost',
-//     user:'root',
-//     password: process.env.PASS,
-//     database:'doghouse_db'
-// })
-
-// connection.connect(function(err){
-//     if(err) throw err;
-//     console.log("Success! Connection registered as id: " + connection.threadId);
-// });
-
-// module.exports = connection;
-
 ////GKF CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL USING SEQUELIZE
 
 // Dependencies
