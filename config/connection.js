5c88702b2b3e69bbb793bc3fd26a55757a4372c8
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-02-27 12:00:19 -0500

    seeds and connection update

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 ////GKF CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL USING SEQUELIZE
-
+require("dotenv").config();
 // Dependencies
 const Sequelize = require('sequelize');
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize('dogHouse_db', 'root', 'chocolate1', {
+const sequelize = new Sequelize('dogHouse_db', 'root', process.env.PASS, {
   host: 'localhost',
   port: 3306,
   dialect: 'mysql',
