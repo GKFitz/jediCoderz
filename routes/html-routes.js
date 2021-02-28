971b88113d9c3ab68fb92d33fd7cb89d40af8d84
Genis Bardales <genis96@yahoo.com>
2021-02-27 22:38:49 -0500

    changed the html routes to the new one - WORKS ON LOAD

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,39 +1,27 @@
-const db = require("../config/connection.js");
+const db = require("../models");
 
-function routes(app) {
+module.exports = (app) => {
+    
     app.get('/', (req,res) => {
         res.render('index', {layout: 'main'});
     });
-    
     app.get('/login', (req,res) => {
         res.render('login', {layout: 'main'});
     });
-    
     app.get('/team', (req,res) => {
         res.render('team', {layout: 'main'});
     });
-    
     app.get('/story', (req,res) => {
         res.render('story', {layout: 'main'});
     });
-    
     app.get('/faq', (req,res) => {
         res.render('faq', {layout: 'main'});
     });
-    
     app.get('/registration', (req,res) => {
         res.render('registration', {layout: 'main'});
     });
-    
     app.get('/my-account', async (req,res) => {
-       db.query("select * from dogs", function(err, data) {
-           res.render('admin', {layout: 'main', dogs: data});
-       });
+       db.Dogs.findAll()
         // console.log(data);
-       
-    });
-    
+    })
 }
-
-
-module.exports = routes;
