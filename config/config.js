5e139900f9a06cdccce520948711e169fc64806c
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-02 19:00:52 -0500

    heroku install, blah

--- config/config.js
+++ config/config.js
@@ -20,6 +20,7 @@ module.exports = {
       "dialect": "mysql"
     },
     "production": {
+      "use_env_variable": "JAWSDB_URL",
       "username": "root",
       "password": process.env.PASS,
       "database": "database_production",
